0194a28c8876f564a557d92d05536f00ba1aa734
alex <alex@pdp7.net>
2023-04-09 22:19:05 +0200

    Add build_container

--- builder.py
+++ builder.py
@@ -2,13 +2,18 @@ import pathlib
 import shutil
 import subprocess
 import tempfile
+import textwrap
 import urllib.request
 
 
 def _sp(args):
+    args = list(map(str, args))
     print(" ".join(args))
     subprocess.run(args, check=True)
 
+
+def _(s):
+    return textwrap.dedent(s).lstrip()
 
 def build_builder_container():
     _sp(["podman", "build",
@@ -17,7 +22,7 @@ def build_builder_container():
         "-t", "aurbuilder:latest"])
 
 
-def build_aur(name: str, dest: pathlib.Path):
+def build_aur(name: str, dest: pathlib.Path) -> pathlib.Path:
     url = f"https://aur.archlinux.org/cgit/aur.git/snapshot/{name}.tar.gz"
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir = pathlib.Path(temp_dir)
@@ -58,4 +63,39 @@ def build_aur(name: str, dest: pathlib.Path):
         generated_zsts = list((temp_dir / name).glob("*.zst"))
         assert len(generated_zsts) == 1
         generated_zst = generated_zsts[0]
-        shutil.copyfile(generated_zst, dest / generated_zst.name)
+        dest_zst = dest / generated_zst.name
+        shutil.copyfile(generated_zst, dest_zst)
+        return dest_zst
+
+
+def build_container(aur_packages, image):
+    build_builder_container()
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir = pathlib.Path(temp_dir)
+
+        container_def = _("""
+        FROM docker.io/library/archlinux:latest
+        """)
+
+        package_files = []
+        for package in aur_packages:
+            package_file = build_aur(package, temp_dir).name
+            package_files.append(f"/pkgs/{package_file}")
+
+        package_files = " ".join(package_files)
+
+        container_def += _(f"""
+        RUN pacman -U {package_files} --noconfirm
+        """)
+
+        container_file_path = temp_dir / "Containerfile"
+        with open(container_file_path, "w") as c:
+            c.write(container_def)
+
+        _sp(["podman", "build",
+             "--cap-add", "sys_chroot",
+             "-f", container_file_path.absolute(),
+             "-t", image,
+             "-v", f"{temp_dir.absolute()}:/pkgs",
+             "--security-opt", "label=disable",])
