3789876f0bd710f355fd213e2acdd3fb83b83646
alex <alex@pdp7.net>
2023-04-09 22:00:27 +0200

    Initial add

---
+++ builder.py
@@ -0,0 +1,61 @@
+import pathlib
+import shutil
+import subprocess
+import tempfile
+import urllib.request
+
+
+def _sp(args):
+    print(" ".join(args))
+    subprocess.run(args, check=True)
+
+
+def build_builder_container():
+    _sp(["podman", "build",
+        "--cap-add", "sys_chroot",
+        "-f", "Containerfile.aurbuilder",
+        "-t", "aurbuilder:latest"])
+
+
+def build_aur(name: str, dest: pathlib.Path):
+    url = f"https://aur.archlinux.org/cgit/aur.git/snapshot/{name}.tar.gz"
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir = pathlib.Path(temp_dir)
+        tarball_path = temp_dir / "tarball"
+        with open(tarball_path, "wb") as tarball:
+            shutil.copyfileobj(urllib.request.urlopen(url), tarball)
+
+        try:
+            # Note that -v:...:...:U chmods everything inside that path to
+            # belong to the builder user, which needs to be reverted.
+            # This is reverted in the finally statement.
+            _sp(["podman", "run",
+                 "--security-opt", "label=disable",
+                 "--rm",
+                 "-v", f"{temp_dir.absolute()}:/builder/tmp:U",
+                 "-w", f"/builder/tmp",
+                 "aurbuilder:latest",
+                 "tar", "-xzvf", "tarball"])
+
+            _sp(["podman", "run",
+                 "--security-opt", "label=disable",
+                 "--rm",
+                 "-v", f"{(temp_dir / name).absolute()}:/builder/{name}",
+                 "-w", f"/builder/{name}",
+                 "aurbuilder:latest",
+                 "makepkg"])
+
+        finally:
+            _sp(["podman", "run",
+                 "--security-opt", "label=disable",
+                 "--rm",
+                 "-v", f"{temp_dir.absolute()}:/builder/tmp:U",
+                 "-w", f"/builder/tmp",
+                 "-u", "root",
+                 "aurbuilder:latest",
+                 "true"])
+
+        generated_zsts = list((temp_dir / name).glob("*.zst"))
+        assert len(generated_zsts) == 1
+        generated_zst = generated_zsts[0]
+        shutil.copyfile(generated_zst, dest / generated_zst.name)
